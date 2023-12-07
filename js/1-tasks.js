60a10dbac16b14f1d3d230c27ad12e9001967f97
Artem Lipchansky <lipchanskiy.an@gmail.com>
2023-12-07 20:00:49 +0200

    added

---
+++ js/1-tasks.js
@@ -0,0 +1,33 @@
+// if (3 > 7) {
+    
+//     console.log('true');
+
+// } else {
+// console.log('false');
+
+// }
+
+
+
+// if (6 > 8) {
+//     console.log('6 > 8');
+// } else if (value > 6) {
+//     console.log(object);
+// }
+
+
+const value = 4
+
+if (value > 5) {
+    console.log('4>5');
+
+} else if (value > 6) {
+    console.log('4>5');
+
+} else if (value > 3) {
+    console.log('4>3');
+
+    console.log('else');
+    
+}
+
