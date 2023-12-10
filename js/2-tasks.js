4f08d05b6bd980d457da6e1c2969a9e77d4b03a3
Artem Lipchansky <lipchanskiy.an@gmail.com>
2023-12-10 23:53:05 +0200

    added

---
+++ js/2-tasks.js
@@ -0,0 +1,11 @@
+// const courseTopic = "JavaScript essentials";
+
+// // Оголошення та ініціалізація змінних
+// const courseTopicLength = courseTopic.length;
+// const firstElement = courseTopic[0];
+// const lastElement = courseTopic[courseTopicLength - 1];
+
+// // Виведення результатів
+// console.log(courseTopicLength); // Виведе 21
+// console.log(firstElement);      // Виведе "J"
+// console.log(lastElement);       // Виведе "s"
